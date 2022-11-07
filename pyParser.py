5435bd445f736e20242e6213c5e9a9dd875127c5
Daniel C. Lechok <dclechok@gmail.com>
2022-11-07 11:07:11 -0500

    added comments

--- pyParser.py
+++ pyParser.py
@@ -3,12 +3,13 @@ import csv
 lanliteDict = {}
 with open('lanlite-ips.csv', newline='') as csvfile:
     spamreader = csv.DictReader(csvfile)
-    counter = 0
+    # build a dictionary of ip addresses, and whether or not they are lanlite
     for row in spamreader:
         # print(row["Version"])
         if row["Version"].find('lanlite') != -1:
             lanliteDict.update({ row["IP address"]: "Lanlite"})
 
+# build a new CSV file with key/values within dictionary
 with open('lanlites.csv', 'w', newline='') as csvfile:
     fieldnames = ['IP Address', 'Lanlite']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
